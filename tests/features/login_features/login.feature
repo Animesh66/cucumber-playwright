d54d2d570b06c3b4bf7cb2278d34c8622c722285
Animesh66 <er.animesh6@hotmail.com>
2025-11-21 16:23:47 +1100

    Updated the fiels.

---
+++ tests/features/login_features/login.feature
@@ -0,0 +1,11 @@
+Feature: Login Functionality
+    As a registered user
+    I want to log in to the application
+    So that I can access my account and personalized features
+    
+    Scenario: Successful login with valid credentials
+        Given I am on the login page
+        When I enter valid credentials
+        And I click the login button
+        Then I should be redirected to the dashboard
+        And I should see a welcome messagen
---
+++ tests/steps/login_steps.ts
@@ -0,0 +1,22 @@
+import { Given, When, Then } from '@cucumber/cucumber';
+    
+    Given('I am on the login page', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+    When('I enter valid credentials', async function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+    When('I click the login button', async function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+    Then('I should be redirected to the dashboard', async function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+    Then('I should see a welcome message', async function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
